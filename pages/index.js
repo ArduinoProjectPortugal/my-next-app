4900dd88426fb3093ed7729fc109a9f53ab69de2
ArduinoProjectPortugal <carpinfkt@hotmail.com>
2021-07-23 12:53:04 +0100

    deploy with git

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Deployed by CarlitoDEV
         </h1>
 
         <p className={styles.description}>
